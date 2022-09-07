a83c1acde6eadee52d474c7af8034cc4523f34eb
Christopher Needham <christopher.needham09@gmail.com>
2022-09-06 23:25:31 -0500

    first commit

---
+++ VariablesAndObjects.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.christopherneedham.week1;
+
+/**
+ * @author 17015
+ *
+ */
+public class VariablesAndObjects {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create a variable to hold the quantity of available plane seats left on a flight
+		int availableSeats = 8;
+		//create a variable to hold the cost of groceries at checkout
+		double groceriesCost = 10.99;
+
+
+		//create a variable to hold a person's middle initial
+		char middleInitial = 'C';
+
+
+		//create a variable to hold true if it's hot outside and false if it's cold outside
+		boolean isHot = true;
+
+		//create a variable to hold a customer's first name
+		String customerName = "Bob";
+
+		//create a variable to hold a street address
+		String streetAddress = "1209 Ashford Dr, Cheyenne WY 58582";
+
+		//print all variables to the console
+		System.out.println(streetAddress);
+		System.out.println(customerName);
+		System.out.println(isHot);
+		System.out.println(middleInitial);
+		System.out.println(groceriesCost);
+		System.out.println(availableSeats);
+		
+
+
+
+		//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+		availableSeats = availableSeats - 2;
+		
+
+		//impulse candy bar purchase, add 2.15 to the grocery total
+		groceriesCost = groceriesCost + 2.15;
+
+
+		//birth certificate was printed incorrectly, change the middle initial to something else
+		middleInitial = 'B';
+
+		//the season has changed, update the hot outside variable to be opposite of what it was
+		isHot = false;
+
+		//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+		String fullName = customerName + " " + middleInitial + " " + "Thompson";
+
+
+		//print a line to the console that introduces the customer and says they live at the address variable
+		System.out.println("Hello,");
+		System.out.println("This is " + fullName + ". He lives in the green house at " + streetAddress);
+
+
+	}
+
+}
